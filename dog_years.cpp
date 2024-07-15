83be3db33ecf8ec376085e2a2b8db49997320614
mdeshon82 <mjdeshon82@gmail.com>
2024-07-15 12:16:36 -0400

    Initial Commit

---
+++ dog_years.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  int dog_age = 6;
+  int early_years = 21;
+  int later_years;
+  int human_years;
+  later_years = (dog_age - 2) * 4;
+  human_years = early_years + later_years;
+  std::cout << "My name is Loki! Ruff ruff, I am " << human_years << " years old in human years.\n";
+}
+
+
+
+
+
+
+
